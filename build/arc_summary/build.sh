3c5c153147d0d965e643142632bbd78f52178d66
Patkos Csaba <patkoscsaba@gmail.com>
2014-12-16 14:01:28 +0200

    STORAGE-1198 Add arc_summary package

---
+++ build/arc_summary/build.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=arc_summary
+VER=0.3
+PKG=system/arc_summary
+SUMMARY="Program to show details about ARC"
+DESC="$SUMMARY"
+
+DEPENDS_IPS="SUNWcs runtime/perl"
+
+build() {
+    pushd $TMPDIR/$BUILDDIR > /dev/null
+    logmsg "Installing ${PROG}"
+    logcmd install -d -m 755 ${DESTDIR}/${PREFIX}
+    logcmd install -m 755 ${PROG}.pl ${DESTDIR}/${PREFIX}/${PROG}
+    popd > /dev/null
+}
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+strip_install
+make_package
+clean_up
