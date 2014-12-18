d26d925cd50a3e983021140123d192db4ef7b439
Patkos Csaba <patkoscsaba@gmail.com>
2014-12-18 16:11:14 +0200

    STORAGE-1198 Collectd compile for gnu99 and preset some paths.

--- build/collectd/build.sh
+++ build/collectd/build.sh
@@ -34,13 +34,14 @@ SUMMARY="Collectd data collection service"
 DESC="$SUMMARY"
 
 DEPENDS_IPS="SUNWcs"
-
+CC="gcc -std=gnu99"
 CONFIGURE_OPTS="--without-libnetsnmp --enable-write_graphite"
+ac_aux_dir=./libltdl/config
 
 collectd_build() {
     pushd $TMPDIR/$BUILDDIR > /dev/null
     logmsg "Reconfiguring ${PROG}"
-    logcmd autoreconf
+    logcmd autoreconf -sfiv
     popd > /dev/null
     build
 }
