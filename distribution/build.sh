029012caa888e62a7955c4e0dd309131f8d4c85f
Patkos Csaba <patkoscsaba@gmail.com>
2015-02-03 11:43:52 +0200

    STORAGE-1348 Log distro build script output.

--- distribution/build.sh
+++ distribution/build.sh
@@ -2,6 +2,8 @@
 
 DIR=`dirname $0`
 CUSTOM_SCRIPT=`readlink -f $DIR/scripts/customize.sh`
+exec > >(tee build.log)
+exec 2>&1
 
 if ! pkg info distribution-constructor >/dev/null 2>&1; then
 	sudo pkg install -v distribution-constructor
@@ -13,3 +15,9 @@ fi
 sed -i -r 's/(.*syneto-storage-[0-9\.]+-)([0-9]+)(.*)/printf "%s%d%s" "\1" "$((\2+1))" "\3"/ge' ${DIR}/text-install-images-build.xml
 
 time sudo distro_const build text-install-images-build.xml
+
+if [ $? -eq 0 ]; then
+	echo "Done"
+else
+	echo "Error. Something went wrong. Check build.log"
+fi
