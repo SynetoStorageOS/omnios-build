69a4eb76ed02abc56ee3fbab12838d43c0b89b7c
Patkos Csaba <patkoscsaba@gmail.com>
2015-02-05 15:23:24 +0200

    STORAGE-1353 Add "pyparsing" dependency to graphite-api.

--- build/graphite-api/build.sh
+++ build/graphite-api/build.sh
@@ -41,7 +41,10 @@ DATA_ROOT=/var/storage
 DATA_DIR=${DATA_ROOT}/statistics
 LIBDIR=${PREFIX}/lib/python2.6/vendor-packages
 
-RUN_DEPENDS_IPS="user/admin library/python-2/gunicorn-26 library/python-2/cairocffi-26"
+RUN_DEPENDS_IPS="user/admin
+	library/python-2/gunicorn-26
+	library/python-2/cairocffi-26
+	library/python-2/pyparsing-26"
 
 install_configuration_files() {
 	logcmd install -d -m 755 ${DESTDIR}/${DATA_ROOT}
