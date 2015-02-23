40a0acd271052ae33b794df87c617791c4c13a37
Patkos Csaba <patkoscsaba@gmail.com>
2015-02-23 13:39:23 +0200

    STORAGE-1378 Add libmemcached package.

---
+++ build/libmemcached/build.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/bash
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License, Version 1.0 only
+# (the "License").  You may not use this file except in compliance
+# with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+#
+# Copyright 2011-2012 OmniTI Computer Consulting, Inc.  All rights reserved.
+# Copyright 2015 Syneto LTD.  All rights reserved.
+# Use is subject to license terms.
+#
+# Load support functions
+. ../../lib/functions.sh
+
+PROG=libmemcached
+VER=1.0.18
+PKG=system/libmemcached
+SUMMARY="libMemcached is an open source C/C++ client library and tools for the memcached server"
+DESC="$SUMMARY"
+
+DEPENDS_IPS="SUNWcs"
+
+init
+download_source $PROG $PROG $VER
+patch_source
+prep_build
+build
+make_isa_stub
+strip_install
+make_package
+clean_up
