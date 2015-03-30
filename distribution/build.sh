7aa92816edb2823286b1d7ad48140223f09c0d97
Patkos Csaba <patkoscsaba@gmail.com>
2015-03-30 10:46:21 +0300

    STORAGE-1433 / STORAGE-1439 Post process text-install-images.xml and dinamically specify the new path to text.xml

--- distribution/build.sh
+++ distribution/build.sh
@@ -1,18 +1,15 @@
 #!/usr/bin/env bash
 
-DIR=`dirname $0`
-CUSTOM_SCRIPT=`readlink -f $DIR/scripts/customize.sh`
+SCRIPT_PATH=`realpath $0`
+DIR=`dirname $SCRIPT_PATH`
 exec > >(tee build.log)
 exec 2>&1
 
-if ! pkg info distribution-constructor >/dev/null 2>&1; then
-	sudo pkg install -v distribution-constructor
-fi
+sudo pkg update -v distribution-constructor
 
-if [ ! -e text-install-images-build.xml ]; then
-	cp -a text-install-images.xml text-install-images-build.xml
-fi
+cp -a text-install-images.xml text-install-images-build.xml
 sed -i -r 's/(.*syneto-storage-[0-9\.]+-)([0-9]+)(.*)/printf "%s%d%s" "\1" "$((\2+1))" "\3"/ge' ${DIR}/text-install-images-build.xml
+sed -i -e "s#__BUILDPATH__#${DIR}#" ${DIR}/text-install-images-build.xml
 
 time sudo distro_const build text-install-images-build.xml
 
